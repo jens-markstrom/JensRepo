01f9095a9ae4d8770412b1103df87e3a677a67ab
Nenad Antic <nenad.antic@ericsson.com>
2012-12-19 15:39:17 +0100

    Updated neverending perl hack

--- forEver.pl
+++ forEver.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 use strict;
 while ( 1 ){
-	open FILE, ">> /local/home/emarald/jens-repo/JensRepo/README" or die "exit";
+	open FILE, ">> /local/home/emarald/jens-repo/JensRepo/README" or die "trying";
 	print FILE "JENS jobba snabbare\n";
 	close FILE;
-	system("/app/git/1.8.0.2/LMWP2/bin/git add .");
-	system("git commit -m 'haha'");
+	system("git commit -a -m 'haha'");
 	system("git push");
 	#sleep 5;	
 }
+
