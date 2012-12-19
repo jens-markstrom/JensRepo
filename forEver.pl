4a5321e8a9f368ffb7014a3a3f5bae001f3a93b9
Martin Aldrin <martin.aldrin@ericsson.com>
2012-12-19 14:51:05 +0100

    haha

--- forEver.pl
+++ forEver.pl
@@ -4,7 +4,7 @@ while ( 1 ){
 	open FILE, ">> /local/home/emarald/jens-repo/JensRepo/README" or die "exit";
 	print FILE "JENS jobba snabbare\n";
 	close FILE;
-	system("/app/git/1.8.0.2/LMWP2/bin/git add /local/home/emarald/jens-repo/jensRepo/README");
+	system("/app/git/1.8.0.2/LMWP2/bin/git add .");
 	system("git commit -m 'haha'");
 	system("git push");
 	<>;
